0828ad1be2d2f33938eba7c0fecaa9ef50c1feab
dunizb <dunizb@outlook.com>
2016-10-02 19:50:58 +0800

    查询电影完结

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,6 +1,10 @@
+var subjectsUtil = require("../../utils/subjectsUtil.js");
 Page({
   data:{
     // text:"这是一个页面"
+    inputVal:"",
+    movies:[],
+    loadingHidden:true
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
@@ -16,5 +20,24 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
+  },
+  bindKeyInput(event){
+      this.setData({inputVal:event.detail.value});
+  },
+  search(){
+      var page = this;
+      page.setData({loadingHidden:false});
+      wx.request({
+          url:"http://api.douban.com/v2/movie/search?q="+page.data.inputVal,
+          header:{
+              "Content-Type":"application/json"
+          },
+          data:{count:50},
+          success:function(res){
+              var subjects = res.data.subjects;
+              subjectsUtil.processSubjects(subjects);
+              page.setData({"movies":subjects,"loadingHidden":true});
+          }
+      });
   }
 })
